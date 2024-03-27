61e892c27b9571c7759cc5f50d4b538bb478225b
JoaoGuilhermeTP <adm.joaoguilherme@gmail.com>
2024-03-26 23:48:14 -0300

    Updated UserControler and created PostController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,16 @@
             <button>Log Out</button>
         </form>
 
+        <div style="border: 3px solid black;">
+            <h2>Create a New Post</h2>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="post title">
+                <textarea name="body" placeholder="body content..."></textarea>
+                <button>Save post</button>
+            </form>
+        </div>
+
     <!-- In case it's not -->
     @else
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 // When route is the index (/), return the "home" view, using an 
@@ -9,14 +10,16 @@ Route::get('/', function () {
     return view('home');
 });
 
-// When route is /register, will execute the function "register"
-// from the "UserController"
+//USER CONTROLER
+
+// Register new user
 Route::post('/register', [UserController::class, 'register']);
-
-// When route is /login, will execute the function "login"
-// from the "UserController"
+// Log user in
 Route::post('/login', [UserController::class, 'login']);
-
-// When route is /logout, will execute the function "logout"
-// from the "UserController"
+// Log user out
 Route::post('/logout', [UserController::class, 'logout']);
+
+//POST CONTROLLER
+
+// Create new post
+Route::post('create-post', [PostController::class, 'createPost'];)
